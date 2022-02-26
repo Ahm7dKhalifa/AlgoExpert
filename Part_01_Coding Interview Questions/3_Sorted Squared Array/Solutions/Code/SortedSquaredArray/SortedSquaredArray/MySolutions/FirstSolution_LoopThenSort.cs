aeb1d999a1006530f24160525fe4bc559e031522
ahmed khalifa <ahm7dkhalifa@gmail.com>
2022-02-26 19:07:14 +0200

    ADD Part_01_Coding Interview Questions\3_Sorted Squared Array Folder

---
+++ Part_01_Coding Interview Questions/3_Sorted Squared Array/Solutions/Code/SortedSquaredArray/SortedSquaredArray/MySolutions/FirstSolution_LoopThenSort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedSquaredArray.MySolutions
+{
+    public class FirstSolution_LoopThenSort
+    {
+        /* Algorithm Analysis :
+         * 
+         * Time Complexity = 
+         * Loop  = n
+         * Sort  = n log n
+         * O(t)  = n + n log n ---> n log n
+         * 
+         * Space Complexity : 
+         * based on the requirement of the problem
+         * O(s) = n when we create new array -> SequradArray
+         * O(s) = 1 when we use same array directly
+         * */
+        public int[] SortedSquaredArray(int[] array)
+        {
+            int[] SequradArray = null;
+            
+            if (array != null)
+            {
+                SequradArray = new int[array.Length];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    SequradArray[i] = array[i] * array[i];
+                }
+
+                Array.Sort(SequradArray);
+            }
+           
+            return SequradArray;
+        }
+
+        /*
+        public int[] SortedSquaredArray(int[] array)
+        {
+            if (array != null)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = array[i] * array[i];
+                }
+
+                Array.Sort(array);
+            }
+
+            return SequradArray;
+        }
+        */
+    }
+}
