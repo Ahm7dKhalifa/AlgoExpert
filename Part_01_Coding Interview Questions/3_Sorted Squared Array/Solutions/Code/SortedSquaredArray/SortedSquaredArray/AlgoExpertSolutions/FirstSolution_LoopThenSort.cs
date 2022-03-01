9d6d10d5e9039450599df17f743051cae2f3bcaf
ahmed khalifa <ahm7dkhalifa@gmail.com>
2022-03-01 12:25:24 +0200

    Edit Part_01_Coding Interview Questions\3_Sorted Squared Array Folder

---
+++ Part_01_Coding Interview Questions/3_Sorted Squared Array/Solutions/Code/SortedSquaredArray/SortedSquaredArray/AlgoExpertSolutions/FirstSolution_LoopThenSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedSquaredArray.AlgoExpertSolutions
+{
+    // Copyright © 2022 AlgoExpert LLC. All rights reserved.
+    public class FirstSolution_LoopThenSort
+    {
+        // O(nlogn) time | O(n) space - where n is the length of the input array
+        public int[] SortedSquaredArray(int[] array)
+        {
+            int[] sortedSquares = new int[array.Length];
+            for (int idx = 0; idx < array.Length; idx++)
+            {
+                int value = array[idx];
+                sortedSquares[idx] = value * value;
+            }
+            Array.Sort(sortedSquares);
+            return sortedSquares;
+        }
+
+    }
+}
---
+++ Part_01_Coding Interview Questions/3_Sorted Squared Array/Solutions/Code/SortedSquaredArray/SortedSquaredArray/AlgoExpertSolutions/SecondSolution_UsingTwoIndexes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedSquaredArray.AlgoExpertSolutions
+{
+    // Copyright © 2022 AlgoExpert LLC. All rights reserved.
+    public class SecondSolution_UsingTwoIndexes
+    {
+        // O(n) time | O(n) space - where n is the length of the input array
+        public int[] SortedSquaredArray(int[] array)
+        {
+            int[] sortedSquares = new int[array.Length];
+            int smallerValueIdx = 0;
+            int largerValueIdx = array.Length - 1;
+            for (int idx = array.Length - 1; idx >= 0; idx--)
+            {
+                int smallerValue = array[smallerValueIdx];
+                int largerValue = array[largerValueIdx];
+                if (Math.Abs(smallerValue) > Math.Abs(largerValue))
+                {
+                    sortedSquares[idx] = smallerValue * smallerValue;
+                    smallerValueIdx++;
+                }
+                else
+                {
+                    sortedSquares[idx] = largerValue * largerValue;
+                    largerValueIdx--;
+                }
+            }
+            return sortedSquares;
+        }
+
+
+    }
+}
