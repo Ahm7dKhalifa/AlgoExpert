5d121fc41011808dd277f21ebff431d68fca3638
ahmed khalifa <ahm7dkhalifa@gmail.com>
2022-03-16 14:30:18 +0200

    ADD Part_01_Coding Interview Questions\01_Arrays\02_Medium\01_ThreeNumberSum Folder

---
+++ Part_01_Coding Interview Questions/01_Arrays/02_Medium/01_ThreeNumberSum/Solutions/Code/ThreeNumberSum/MySolutions/FirstSolution_UsingThreeLoops.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeNumberSum.MySolutions
+{
+    public class FirstSolution_UsingThreeLoops
+    {
+        /* algorithm analysis :
+         * Time Complexity : O(N3)   ---> Three Loops
+         * Space Complexity : O(N)
+         */
+        public static List<int[]> ThreeNumberSum(int[] array, int targetSum)
+        {
+            int firstNumber;
+            int secondNumber;
+            int thirdNumber;
+            int currentSum;
+            List<int[]> result = new List<int[]>();
+
+            //this Sorting step in current Solution is not required to solve the problem 
+            //but required to pass automation tests case only
+            //ex : same numbers with different orders causes problem :
+            //expected :
+            //[
+            //[-8, 2, 6],
+            //[-8, 3, 5],
+            // [-6, 1, 5]
+            //]
+            //actual :
+            //[
+            //[3, 5, -8],
+            //[1, -6, 5],
+            //[2, -8, 6]
+            //]
+            Array.Sort(array);
+
+            if (array.Length >= 2)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    firstNumber = array[i];
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        secondNumber = array[j];
+                        for (int k = j + 1; k < array.Length; k++)
+                        {
+                            thirdNumber = array[k];
+
+                            currentSum = firstNumber + secondNumber + thirdNumber;
+
+                            if (currentSum == targetSum)
+                            {
+                                int[] threeNumbers = { firstNumber, secondNumber, thirdNumber };
+                                result.Add(threeNumbers);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
---
+++ Part_01_Coding Interview Questions/01_Arrays/02_Medium/01_ThreeNumberSum/Solutions/Code/ThreeNumberSum/MySolutions/SecondSolution_UsingTwoLoopsWithHashTable.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeNumberSum.MySolutions
+{
+    public class SecondSolution_UsingTwoLoopsWithHashTable
+    {
+        /* algorithm analysis :
+         * Time Complexity : 
+         * - Convert Array To Hash Set = N
+         * - Two Loops : N^2
+         * - Total : O(N^2 + N) ~= O(N^2)
+         * 
+         * Space Complexity : O(2N)
+         * Hash Set : N
+         * Result : N
+         * Total = O(2N) ~= O(N)
+         */
+        public static List<int[]> ThreeNumberSum(int[] array, int targetSum)
+        {
+            int firstNumber;
+            int secondNumber;
+            int expectedThirdNumber;
+
+            List<int[]> result = new List<int[]>();
+
+            //this Sorting step in current Solution is not required to solve the problem 
+            //but required to pass automation tests case only
+            //ex : same numbers with different orders causes problem :
+            //expected :
+            //[
+            //[-8, 2, 6],
+            //[-8, 3, 5],
+            // [-6, 1, 5]
+            //]
+            //actual :
+            //[
+            //[3, 5, -8],
+            //[1, -6, 5],
+            //[2, -8, 6]
+            //]
+            Array.Sort(array);
+
+            HashSet<int> HashSet = array.ToHashSet();
+            if (array.Length >= 2)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    firstNumber = array[i];
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        secondNumber = array[j];
+
+                        expectedThirdNumber = targetSum - (firstNumber + secondNumber);
+
+                        bool isExpectedThirdNumberExist = HashSet.Contains(expectedThirdNumber) ;
+
+                        if (isExpectedThirdNumberExist)
+                        {
+                            int[] threeNumbers = { firstNumber, secondNumber, expectedThirdNumber };
+                            result.Add(threeNumbers);
+                            break;
+                        }
+
+                    }
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
