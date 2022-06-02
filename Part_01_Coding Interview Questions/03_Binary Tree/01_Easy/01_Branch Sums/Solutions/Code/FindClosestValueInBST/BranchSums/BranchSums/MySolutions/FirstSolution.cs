e611f90a76118aaf10d62e3911300c99b5e4db1e
ahmed khalifa <ahm7dkhalifa@gmail.com>
2022-06-02 15:10:24 +0200

    ADD Part_01_Coding Interview Questions\03_Binary Tree\01_Easy\01_Branch Sums Folder

---
+++ Part_01_Coding Interview Questions/03_Binary Tree/01_Easy/01_Branch Sums/Solutions/Code/FindClosestValueInBST/BranchSums/BranchSums/MySolutions/FirstSolution.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchSums.MySolutions
+{
+    public class FirstSolution
+    {
+        #region Algorithm Design
+        /*
+         * - BranchSumsHelper(sumList , currentNode , currentBranchTotalSum)
+         * Base Case
+         * 1- If currentNode == null
+         *    add currentBranchTotalSum To SumList
+         *    currentBranchTotalSum = 0
+         * 
+         * currentBranchTotalSum += currentNode
+         * 
+         * BranchSumsHelper(sumList , currentNode.Left , currentBranchTotalSum)
+         * BranchSumsHelper(sumList , currentNode.Right , currentBranchTotalSum)
+         * 
+         */
+        #endregion
+        #region Algorithm Analysis
+        /*
+         * - BranchSumsHelper(sumList , currentNode , currentBranchTotalSum)
+         * 1- If currentNode == null
+         *    add currentBranchTotalSum To SumList
+         *    currentBranchTotalSum = 0
+         * 
+         * currentBranchTotalSum += currentNode
+         * 
+         * BranchSumsHelper(sumList , currentNode.Left , currentBranchTotalSum)
+         * BranchSumsHelper(sumList , currentNode.Right , currentBranchTotalSum)
+         * 
+         */
+        #endregion
+        #region Algorithm Implementation
+        public static List<int> BranchSums(BinaryTree root)
+        {
+            List<int> TotalSumsOfAllBranchesList = new List<int>();
+            BranchSumsHelper(root, TotalSumsOfAllBranchesList, 0);
+            return TotalSumsOfAllBranchesList;
+        }
+
+        public static void BranchSumsHelper(BinaryTree currentNode , List<int> TotalSumsOfAllBranchesList , int currentTotalBranchSum)
+        {
+            //base case 1
+            if (currentNode  == null )
+            { 
+                return;
+            }
+
+            currentTotalBranchSum += currentNode.value;
+
+            //base case 2 
+            if (IsLeafNode(currentNode))
+            {
+                TotalSumsOfAllBranchesList.Add(currentTotalBranchSum);
+                return;
+            }
+
+           
+
+            BranchSumsHelper(currentNode.left , TotalSumsOfAllBranchesList, currentTotalBranchSum);
+            BranchSumsHelper(currentNode.right, TotalSumsOfAllBranchesList, currentTotalBranchSum);
+
+
+        }
+
+        public static bool IsLeafNode(BinaryTree currentNode)
+        {
+            return currentNode.left == null && currentNode.right == null;
+        }
+
+        public class BinaryTree
+        {
+            public int value;
+            public BinaryTree left;
+            public BinaryTree right;
+
+            public BinaryTree(int value)
+            {
+                this.value = value;
+                this.left = null;
+                this.right = null;
+            }
+        }
+
+      
+        #endregion
+
+    }
+}
---
+++ Part_01_Coding Interview Questions/03_Binary Tree/01_Easy/01_Branch Sums/Solutions/Code/FindClosestValueInBST/BranchSums/BranchSums/Program.cs
@@ -0,0 +1,34 @@
+﻿using BranchSums.MySolutions;
+using System;
+using static BranchSums.MySolutions.FirstSolution;
+
+namespace BranchSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new BinaryTree(1);
+            root.left = new BinaryTree(2);
+            root.right = new BinaryTree(3);
+
+            root.left.left = new BinaryTree(4);
+            root.left.right = new BinaryTree(5);
+
+
+            root.right.left = new BinaryTree(6);
+            root.right.right = new BinaryTree(7);
+
+
+            root.left.left.left = new BinaryTree(8);
+            root.left.left.right = new BinaryTree(9);
+
+
+            root.left.right.left = new BinaryTree(10);
+
+            var result = FirstSolution.BranchSums(root);
+
+            Console.WriteLine();
+        }
+    }
+}
