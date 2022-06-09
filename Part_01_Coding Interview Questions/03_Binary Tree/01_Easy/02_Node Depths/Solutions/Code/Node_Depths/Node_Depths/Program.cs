00e6df9c9a2cc3f6245c04e3ce458c530a832db8
ahmed khalifa <ahm7dkhalifa@gmail.com>
2022-06-09 16:45:01 +0200

    ADD Part_01_Coding Interview Questions\03_Binary Tree\01_Easy\02_Node Depths Folder

---
+++ Part_01_Coding Interview Questions/03_Binary Tree/01_Easy/02_Node Depths/Solutions/Code/Node_Depths/Node_Depths/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Node_Depths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
