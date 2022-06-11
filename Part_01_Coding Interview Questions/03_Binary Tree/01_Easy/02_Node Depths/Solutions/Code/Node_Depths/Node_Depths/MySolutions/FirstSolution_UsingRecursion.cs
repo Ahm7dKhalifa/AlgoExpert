74096d49e3e7ec9703a34c893d64a25d47fd7fc5
ahmed khalifa <ahm7dkhalifa@gmail.com>
2022-06-11 22:04:20 +0200

    UPDATE Part_01_Coding Interview Questions\03_Binary Tree\01_Easy\02_Node Depths\ Folder

---
+++ Part_01_Coding Interview Questions/03_Binary Tree/01_Easy/02_Node Depths/Solutions/Code/Node_Depths/Node_Depths/MySolutions/FirstSolution_UsingRecursion.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node_Depths.MySolutions
+{
+    public class FirstSolution_UsingRecursion
+	{
+		#region Algorithm Design
+		/* - NodeDepthsHelper(BinaryTree CurrentNode , int ParentDepth , NodeDepthsResult Result)
+		    Base Case :
+		    if (CurrentNode == null)
+				return;
+
+		    Business Logic :
+			CurrentNode.CurrentDepth = ParentDepth + 1;
+			Result.TotalSumOfDepths += CurrentNode.CurrentDepth;
+
+		    Recursion Cases :
+			NodeDepthsHelper(CurrentNode.left,  CurrentNode.CurrentDepth , Result);
+			NodeDepthsHelper(CurrentNode.right, CurrentNode.CurrentDepth , Result);
+         */
+		#endregion
+		#region Algorithm Analysis
+		/*
+         * Time Complexity : 
+            Base Case :
+		    if (CurrentNode == null) --> O(1)
+				return;
+
+		    Business Logic :
+			CurrentNode.CurrentDepth = ParentDepth + 1;  --> O(1)
+			Result.TotalSumOfDepths += CurrentNode.CurrentDepth;  --> O(1)
+
+		    Recursion Cases :
+			NodeDepthsHelper(CurrentNode.left,  CurrentNode.CurrentDepth , Result); --> T(n/2)
+			NodeDepthsHelper(CurrentNode.right, CurrentNode.CurrentDepth , Result); --> T(n/2)
+           
+		    total time complexity = O(1) + O(1) + O(1) + T(n/2) + T(n/2)
+                                  = 2T(n/2) + O(3)
+                                  = T(n) + C
+                                  = O(n)
+         
+		 * Space Complexity : O(h)
+           where h is the max height of the tree
+		   when the tree is balanced : h = log n
+		   when the tree is skewed : h = n
+         
+         */
+		#endregion
+		#region Algorithm Implementation
+		public static int NodeDepths(BinaryTree root)
+		{
+			NodeDepthsResult Result = new NodeDepthsResult();
+			NodeDepthsHelper(root, -1, Result);
+			return Result.TotalSumOfDepths;
+		}
+
+		public static void NodeDepthsHelper(BinaryTree CurrentNode , int ParentDepth , NodeDepthsResult Result)
+        {
+			if (CurrentNode == null)
+				return;
+
+			CurrentNode.CurrentDepth = ParentDepth + 1;
+			Result.TotalSumOfDepths += CurrentNode.CurrentDepth;
+
+			NodeDepthsHelper(CurrentNode.left,  CurrentNode.CurrentDepth , Result);
+			NodeDepthsHelper(CurrentNode.right, CurrentNode.CurrentDepth , Result);
+
+		}
+		
+		public class BinaryTree
+		{
+			public int value;
+			public int CurrentDepth;
+			public BinaryTree left;
+			public BinaryTree right;
+
+			public BinaryTree(int value)
+			{
+				this.value = value;
+				left = null;
+				right = null;
+			}
+		}
+
+		public class NodeDepthsResult
+		{
+			public int TotalSumOfDepths;
+		}
+		#endregion
+	}
+}
---
+++ Part_01_Coding Interview Questions/03_Binary Tree/01_Easy/02_Node Depths/Solutions/Code/Node_Depths/Node_Depths/MySolutions/SecondSolution_UsingStack.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node_Depths.MySolutions
+{
+    public class SecondSolution_UsingStack
+	{
+		#region Algorithm Design
+		/* 1- Define Variable To Store TotalSumOfDepths
+         * 2- Define And Create Nodes Stack List
+         * 3- if Root Node is Not Null
+         *       3.1 Push Root Node To Stack
+         *       3.2 While Stack Is Not Empty
+         *                1.Define Variable Called Current Node 
+         *                2.Pop Last Node From Stack And Store it in Current Node Variable
+         *                3.Increment TotalSumOfDepths By Current Node Depth
+         *                4.If Left Node Of Current Node  != Null
+         *                     4.1 Set Depth Of Left Node  = Current Node Depth + 1
+         *                     4.2 Push Left Node To Stack
+         *                5.If Right Node Of Current Node  != Null
+         *                     5.1 Set Depth Of Right Node  = Current Node Depth + 1
+         *                     5.2 Push Right Node To Stack
+         * 
+         * 4- return TotalSumOfDepths
+         * 
+         */
+		#endregion
+		#region Algorithm Analysis
+		/*
+         * Time Complexity : 
+         * - O(N)
+         * 
+         * Space Complexity :
+         * StackNodes =  O(N)
+         * Total Space Complexity = O(N)
+         * 
+         */
+		#endregion
+		#region Algorithm Implementation
+		public static int NodeDepths(BinaryTree root)
+		{
+			int TotalSumOfDepths = 0;
+			Stack<BinaryTree> NodesStack = new Stack<BinaryTree>();
+			if(root != null)
+            {
+				NodesStack.Push(root);
+				while(NodesStack.Count > 0)
+                {
+					BinaryTree CurrentNode = NodesStack.Pop();
+					TotalSumOfDepths = CurrentNode.Depth;
+					AddChildNodeToStack(CurrentNode.left, CurrentNode, NodesStack);
+					AddChildNodeToStack(CurrentNode.right, CurrentNode, NodesStack);
+					/*
+					if(CurrentNode.left != null)
+                    {
+						CurrentNode.left.Depth = CurrentNode.Depth + 1;
+						NodesStack.Push(CurrentNode.left);
+					}
+					if (CurrentNode.right != null)
+					{
+						CurrentNode.right.Depth = CurrentNode.Depth + 1;
+						NodesStack.Push(CurrentNode.right);
+					}
+					*/
+
+
+				}
+
+			}
+			return TotalSumOfDepths;
+		}
+
+		public static void AddChildNodeToStack(BinaryTree childNode, BinaryTree parentNode,Stack<BinaryTree> NodesStack)
+        {
+			if(childNode != null && parentNode != null && NodesStack != null)
+            {
+				childNode.Depth = parentNode.Depth + 1;
+				NodesStack.Push(childNode);
+			}
+        }
+
+		public class BinaryTree
+		{
+			public int value;
+			public int Depth;
+			public BinaryTree left;
+			public BinaryTree right;
+
+			public BinaryTree(int value)
+			{
+				this.value = value;
+				left = null;
+				right = null;
+			}
+		}
+		#endregion
+	}
+}
