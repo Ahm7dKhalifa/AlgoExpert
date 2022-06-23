40667088bf8e8ce65d3041d1bd723263bdc366ac
ahmed khalifa <ahm7dkhalifa@gmail.com>
2022-06-23 14:06:05 +0200

    ADD Part_01_Coding Interview Questions\03_Binary Tree\02_Medium\02_Binary Tree Diameter Folder

---
+++ Part_01_Coding Interview Questions/03_Binary Tree/02_Medium/02_Binary Tree Diameter/Solutions/Code/Binary Tree Diameter/Binary Tree Diameter/MySolutions/FirstSolution.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree_Diameter.MySolutions
+{
+    public class FirstSolution
+    {
+		#region Algorithm Design
+		/*
+		- BinaryTreeDiameterHelper(BinaryTree node , MaxHeightResult MaxHeightResult)
+		 
+		 //Base Case 1 :
+		  if( node == null )
+		      return;
+		 
+		 //Base Case 2 :
+		 if( node.left == null && node.right == null )
+		 ---> CurrentNodeHeight = 1
+		 ---> Return 
+
+		 //Recursion Cases : 
+		 BinaryTreeDiameterHelper( node.left , MaxHeightResult MaxHeightResult)
+		 BinaryTreeDiameterHelper( node.right , MaxHeightResult MaxHeightResult)
+
+		 //Business Logic :
+		 node.CalculateTotalHieght()
+			 int LeftSubreeHeight =  node.left != null ? node.left.CurrentNodeHeight : 0;
+			 int RightSubreeHeight =  node.right != null ? node.right.CurrentNodeHeight : 0;
+			 CurrentNodeHeight = LeftSubreeHeight + RightSubreeHeight
+		 
+		 MaxHeightResult.CalculateMaxHeight(CurrentNodeHeight) 
+		
+		*/
+		#endregion
+		public int BinaryTreeDiameter(BinaryTree tree)
+		{
+			BinaryTreeDiameterHelper(tree);
+			return MaxBinaryTreeDiameterResult.GetMaxDiameter();
+		}
+
+		public void BinaryTreeDiameterHelper(BinaryTree node)
+		{
+			//Base Case 1 :
+			if (node == null)
+				return;
+
+			//Base Case 2 :
+			if (node.left == null && node.right == null)
+            {
+				node.currentHeight = 1;
+				return;
+			}
+
+			//Recursion Cases : 
+			BinaryTreeDiameterHelper(node.left);
+			BinaryTreeDiameterHelper(node.right);
+
+			//Business Logic :
+			node.CalculateTotalSumOfLeftAndRight();
+			MaxBinaryTreeDiameterResult.SetIfMax(node.totalSumOfLeftAndRight);
+		}
+
+		public class BinaryTree
+		{
+			public int value;
+			public BinaryTree left;
+			public BinaryTree right;
+			public int currentHeight;
+			public int leftHeight;
+			public int rightHeight;
+			public int totalSumOfLeftAndRight;
+
+			public BinaryTree(int value)
+			{
+				this.value = value;
+			}
+
+			public void CalculateTotalSumOfLeftAndRight()
+            {
+				leftHeight = this.left != null ? this.left.currentHeight : 0;
+				rightHeight = this.right != null ? this.right.currentHeight : 0;
+				totalSumOfLeftAndRight = leftHeight + rightHeight;
+			}
+
+		
+		}
+
+		public class MaxBinaryTreeDiameterResult
+		{
+			public static int MaxHeight;
+
+			public static void SetIfMax(int currentTotalSumOfLeftAndRight)
+            {
+				if (currentTotalSumOfLeftAndRight > MaxHeight)
+					MaxHeight = currentTotalSumOfLeftAndRight;
+			}
+
+			public static int GetMaxDiameter()
+			{
+				return MaxHeight - 1;
+			}
+
+		}
+	}
+}
---
+++ Part_01_Coding Interview Questions/03_Binary Tree/02_Medium/02_Binary Tree Diameter/Solutions/Code/Binary Tree Diameter/Binary Tree Diameter/Program.cs
@@ -0,0 +1,24 @@
+﻿using Binary_Tree_Diameter.MySolutions;
+using System;
+using static Binary_Tree_Diameter.MySolutions.FirstSolution;
+
+namespace Binary_Tree_Diameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var root = new BinaryTree(1);
+			root.left = new BinaryTree(3);
+			root.left.left = new BinaryTree(7);
+			root.left.left.left = new BinaryTree(8);
+			root.left.left.left.left = new BinaryTree(9);
+			root.left.right = new BinaryTree(4);
+			root.left.right.right = new BinaryTree(5);
+			root.left.right.right.right = new BinaryTree(6);
+			root.right = new BinaryTree(2);
+			FirstSolution firstSolution = new FirstSolution();
+			var actual = firstSolution.BinaryTreeDiameter(root);
+		}
+    }
+}
