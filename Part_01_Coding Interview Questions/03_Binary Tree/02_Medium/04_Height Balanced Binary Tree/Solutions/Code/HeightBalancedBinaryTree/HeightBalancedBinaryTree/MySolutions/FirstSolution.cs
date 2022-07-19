91a87ef413444b4b243c8768806258ec607e86cf
ahmed khalifa <ahm7dkhalifa@gmail.com>
2022-07-19 10:49:00 +0200

    ADD Part_01_Coding Interview Questions\03_Binary Tree\02_Medium\04_Height Balanced Binary Tree Folder

---
+++ Part_01_Coding Interview Questions/03_Binary Tree/02_Medium/04_Height Balanced Binary Tree/Solutions/Code/HeightBalancedBinaryTree/HeightBalancedBinaryTree/MySolutions/FirstSolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeightBalancedBinaryTree.MySolutions
+{
+    public class FirstSolution
+    {
+        #region Algorithm Design 
+        /*
+          
+          //base case 1
+          if(currentNode == null)
+             return 0
+          
+          //business logic
+          var leftHeight = GetHeight(currentNode.Left);
+          var rightHeight = GetHeight(currentNode.Right);
+          result.isTreeHasHeightBalanced(leftHeight,rightHeight)
+            
+          
+
+         */
+        #endregion
+    }
+}
