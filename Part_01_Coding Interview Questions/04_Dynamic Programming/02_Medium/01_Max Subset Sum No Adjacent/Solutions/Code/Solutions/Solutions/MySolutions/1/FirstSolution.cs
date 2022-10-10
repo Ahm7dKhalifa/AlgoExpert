93c18779468cc9ec2edd7738a073e676124f446e
Ahmed Khalifa <a.akhalifa@dev.thiqah.sa>
2022-10-10 19:42:11 +0300

    ADD Part_01_Coding Interview Questions\04_Dynamic Programming\02_Medium\01_Max Subset Sum No Adjacent Folder

---
+++ Part_01_Coding Interview Questions/04_Dynamic Programming/02_Medium/01_Max Subset Sum No Adjacent/Solutions/Code/Solutions/Solutions/MySolutions/1/FirstSolution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions.MySolutions._1
+{
+    public class FirstSolution
+    {
+        public static int MaxSubsetSumNoAdjacent(int[] array)
+        {
+            if (array == null || array.Length == 0)
+                return 0;
+            else if (array.Length == 1)
+                return array[0];
+
+            int[] MaxSums = (int[])array.Clone();
+            MaxSums[1] = Math.Max(array[0], array[1]);
+
+            for (int i = 2; i < array.Length; i++)
+            {
+                MaxSums[i] = Math.Max(MaxSums[i - 1], MaxSums[i - 2] + array[i]);
+            }
+
+            return MaxSums[array.Length - 1];
+        }
+    }
+}
